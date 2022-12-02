a15fef3bc3199d0a978fe0faba31dd50998e6ebe
GomgomKim <dhflwlsjfzz@naver.com>
2022-12-02 15:29:38 +0900

    [Init commit] 1. Start Gomgombank Backend Proj

---
+++ src/main/java/com/gomgom/gomgombank/GomgomBankApplication.java
@@ -0,0 +1,13 @@
+package com.gomgom.gomgombank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GomgomBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GomgomBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gomgom/gomgombank/GomgomBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomgom.gomgombank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomgomBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
