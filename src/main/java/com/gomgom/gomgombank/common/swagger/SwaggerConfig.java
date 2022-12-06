0cc8f706175549d3536046f51937cd6cf9ab541b
GomgomKim <dhflwlsjfzz@naver.com>
2022-12-06 18:45:00 +0900

    [Fix Configuration Error] 1. Fix Configuration Duplicate Error

--- src/main/java/com/gomgom/gomgombank/common/swagger/SwaggerConfig.java
+++ src/main/java/com/gomgom/gomgombank/common/swagger/SwaggerConfig.java
@@ -8,10 +8,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
+//@Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
